ed3af42a02edc802e51f5809886962ab0ffb9e38
Remo Rothacher <rro@nexplore.ch>
2025-12-09 10:23:33 +0100

    refactor: enhance TypedSelectViewSourceConfig type definitions and documentation
    Co-authored-by: Phil J <johnnyelwailer@users.noreply.github.com>

--- ng/practices-ng-list-view-source/src/lib/select-view-source-fluent-builder/types.ts
+++ ng/practices-ng-list-view-source/src/lib/select-view-source-fluent-builder/types.ts
@@ -17,33 +17,56 @@ export type SelectViewSourceFilterType<TData, TLabelKey> = [TLabelKey] extends [
     ? Pick<TData, TLabelKey>
     : SelectViewSourceDefaultFilter;
 
+type InferredlabelKey<TLabelKey, TData> = TLabelKey extends StringKeyOf<TData> ? TLabelKey : StringKeyOf<TData>;
+
 export type TypedSelectViewSourceConfig<
     TData,
     TLabelKey extends SelectViewSourceLabelKey<TData>,
     TQueryParams = TData,
     TOrdering = TData
-> = (TLabelKey extends StringKeyOf<TData>
-    ? {
-          // The label property to display in the select
-          label?: TLabelKey;
-      }
-    : {
-          // The label property to display in the select
-          label?: StringKeyOf<TData>;
-      }) & {
-    // The value property to bind
+> = {
+    /**
+     * The label property to display in the select.
+     *
+     * Will **also** be used to infer the type of the queryParams filter object.
+     * For example:
+     * ```ts
+     * selectViewSource.withConfig({
+     *   label: 'fullName',
+     *   loadFn: (queryParams) => searchByKeywords(queryParams.filter.fullName) // `filter` is type `{ fullName: string }`, where fullName represents the typed keywords.
+     * })
+     * ```
+     *
+     * Note: If you don't provide a label, the filter will fall back to a default type: `{ label: string }`
+     */
+    label?: InferredlabelKey<TLabelKey, TData>;
+
+    /**
+     * The value property to bind
+     */
     value?: keyof TData;
 
-    // True when the select should be searchable, false otherwise. Default is true.
+    /**
+     * True when the select should be searchable, false otherwise. Default is true.
+     */
     searchable?: boolean;
 
-    // True when the select should filter interally using a simple text search, false otherwise. Default is false.
+    /**
+     * True when the select should filter interally using a simple text search, false otherwise. Default is false.
+     */
     localSearch?: boolean;
 
-    /** Type is used solely for type inference. Unfortunately, typescript cannot properly infer the type without it */
+    /**
+     * Type is used solely for type inference. Unfortunately, typescript cannot properly infer the type without it.
+     *
+     * It's completely optional and has no runtime-effect.
+     *
+     * Alternatively, create the select view source using `selectViewSource.withTye<MyItemType>().withConfig(...)
+     */
     type?: Type<TData>;
 
-    /** The query params type is used to infer the type that is used by the query params sent to the server.
+    /**
+     * The query params type is used to infer the type that is used by the query params sent to the server.
      *
      * This affects the available keys for the `orderBy` property.
      *
@@ -51,6 +74,13 @@ export type TypedSelectViewSourceConfig<
      * */
     queryParamsType?: Type<TQueryParams>;
 
+    /**
+     * The function that will be called whenever new data is requested.
+     *
+     * It will be called with the current query params, which includes sorting, paging, and filtering arguments.
+     *
+     * It should return either a promise, observable or directly a ListResult.
+     */
     loadFn: (
         params: IQueryParamsWithFilter<SelectViewSourceFilterType<TData, TLabelKey>>
     ) => MaybeAsync<Partial<IListResult<TData>>>;
