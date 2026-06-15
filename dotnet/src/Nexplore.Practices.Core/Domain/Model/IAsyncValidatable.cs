020777ea6459a6aac8547078e4d5ae31e84b6822
Stefan Stegmüller <stefan.stegmueller@nexplore.ch>
2026-06-15 15:07:04 +0200

    remove suppress attribute

--- dotnet/src/Nexplore.Practices.Core/Domain/Model/IAsyncValidatable.cs
+++ dotnet/src/Nexplore.Practices.Core/Domain/Model/IAsyncValidatable.cs
@@ -1,12 +1,10 @@
 namespace Nexplore.Practices.Core.Domain.Model
 {
     using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
     using System.Threading;
     using System.Threading.Tasks;
     using Nexplore.Practices.Core.Validation;
 
-    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "Used as marker for all validatable entities, generic and non generic types necessary.")]
     public interface IAsyncValidatable<in TValidationContext> : IValidatable
     {
         Task<IReadOnlyCollection<EntityValidationError>> ValidateAsync(TValidationContext context, CancellationToken cancellationToken);
