a60b11b801d45c2ec31523ae91ae9a83015ec0af
Philip Jonientz <rocka@MacBook-Pro.local>
2026-09-08 11:05:04 +0200

    fix(test): use supported Angular Jest preset

--- ng/practices-ng-forms-signal-forms/angular-22/jest.config.ts
+++ ng/practices-ng-forms-signal-forms/angular-22/jest.config.ts
@@ -1,22 +1,15 @@
 import type { Config } from 'jest';
+import { createCjsPreset } from 'jest-preset-angular/presets/index.js';
 
 const config: Config = {
+    ...createCjsPreset({
+        tsconfig: '<rootDir>/angular-22/tsconfig.spec.json',
+    }),
     displayName: 'forms-signal-forms-angular-22',
     rootDir: '..',
     testEnvironment: 'jsdom',
     testMatch: ['<rootDir>/src/**/*.spec.ts'],
     setupFilesAfterEnv: ['<rootDir>/angular-22/test-setup.ts'],
-    transform: {
-        '^.+\\.(ts|mjs|js|html)$': [
-            'jest-preset-angular',
-            {
-                tsconfig:
-                    '<rootDir>/angular-22/tsconfig.spec.json',
-                stringifyContentPathRegex: '\\.(html|svg)$',
-            },
-        ],
-    },
-    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
 
 export default config;
