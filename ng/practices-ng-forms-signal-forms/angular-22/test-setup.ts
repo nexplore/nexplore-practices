92d786529cdef6ea3284f754f9142f9fb0701442
Philip Jonientz <rocka@MacBook-Pro.local>
2026-09-08 11:41:39 +0200

    fix(test): register Angular compiler facade

--- ng/practices-ng-forms-signal-forms/angular-22/test-setup.ts
+++ ng/practices-ng-forms-signal-forms/angular-22/test-setup.ts
@@ -1,4 +1,13 @@
-import '@angular/compiler';
+import { CompilerFacadeImpl } from '@angular/compiler';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+const angularGlobal = globalThis as typeof globalThis & {
+    ng?: {
+        ɵcompilerFacade?: CompilerFacadeImpl;
+    };
+};
+
+angularGlobal.ng ??= {};
+angularGlobal.ng.ɵcompilerFacade ??= new CompilerFacadeImpl();
 
 setupZoneTestEnv();
